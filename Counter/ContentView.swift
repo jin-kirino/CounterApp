a54d31cb16e6b6c94e339791e5d5668fce70d034
jin-kirino <jinkirino@gmail.com>
2023-05-21 23:18:16 +0900

    レイアウト作成

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                Color.orange
+                    .ignoresSafeArea()
+                Button {
+                    print("+1")
+                } label: {
+                    Text("")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .padding(.bottom, 10)
+                }
+                VStack {
+                    Spacer()
+                    Text("\(count)")
+                        .font(.system(size: 90))
+                        .padding(.bottom, 20)
+                    Spacer()
+                    HStack {
+                        Button {
+                            print("-1")
+                        } label: {
+                            Text("ー")
+                                .font(.title)
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(Color.black)
+                                .overlay(
+                                    Circle()
+                                        .stroke(Color.green, lineWidth: 3)
+                                )
+                                .padding()
+                        }
+                        Spacer()
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
