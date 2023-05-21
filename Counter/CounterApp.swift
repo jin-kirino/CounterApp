24a37ad8814844b8e8992d0733d4dcfca983fc28
jin-kirino <jinkirino@gmail.com>
2023-05-21 21:18:27 +0900

    first push

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by 神　樹里乃 on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
