d0ddc6ebd8647512d8d2638a58c8df11b13e4e81
jin-kirino <jinkirino@gmail.com>
2023-05-23 07:20:05 +0900

    １の位によって背景色を変える

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ZStack {
-                Color.orange
+                BackgroundView(count: count)
                     .ignoresSafeArea()
                 Button {
                     addOneCount()
@@ -56,6 +56,34 @@ struct ContentView: View {
 
     private func MinusOneCount() {
         count -= 1
+    }
+}
+
+struct BackgroundView: View {
+    let count: Int
+
+    var body: some View {
+        if count % 10 == 0 {
+            Color.black
+        } else if count % 10 == 1 {
+            Color.red
+        } else if count % 10 == 2 {
+            Color.gray
+        } else if count % 10 == 3 {
+            Color.blue
+        } else if count % 10 == 4 {
+            Color.green
+        } else if count % 10 == 5 {
+            Color.orange
+        } else if count % 10 == 6 {
+            Color.yellow
+        } else if count % 10 == 7 {
+            Color.brown
+        } else if count % 10 == 8 {
+            Color.cyan
+        } else if count % 10 == 9 {
+            Color.indigo
+        }
     }
 }
 
