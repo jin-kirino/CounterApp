334dd614703f198ec2fba775b3da467b048188ec
jin-kirino <jinkirino@gmail.com>
2023-05-28 18:06:29 +0900

    オリジナルカラーを追加

---
+++ Counter/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  Counter
+//
+//  Created by 神　樹里乃 on 2023/05/28.
+//
+
+import Foundation
---
+++ Counter/View/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  Counter
+//
+//  Created by 神　樹里乃 on 2023/05/28.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
