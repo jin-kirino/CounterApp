418c6ca1789367f2d7adcbf877487db0adcd6636
jin-kirino <jinkirino@gmail.com>
2023-06-03 12:31:59 +0900

    背景色のバリエーションを設定

--- Counter/View/BackgroundView.swift
+++ Counter/View/BackgroundView.swift
@@ -8,33 +8,73 @@
 import SwiftUI
 
 struct BackgroundView: View {
+    @ObservedObject var counterModel = CounterModel()
     let count: Int
-//    let isSetGradation: Bool
 
     var body: some View {
-        switch count % 10 {
-        case 0:
-            Color.black
-        case 1:
-            Color.red
-        case 2:
-            Color.gray
-        case 3:
-            Color.blue
-        case 4:
-            Color.green
-        case 5:
-            Color.orange
-        case 6:
-            Color.yellow
-        case 7:
-            Color.brown
-        case 8:
-            Color.cyan
-        case 9:
-            Color.indigo
-        default:
-            Color.clear
+        if counterModel.isChangeBackgroundColor {
+            if counterModel.isGradation {
+                // 色変えるグラデーション
+                switch count % 10 {
+                case 0:
+                    Color.lemonYellToSkyBlue
+                case 1:
+                    Color.pinkToPurple
+                case 2:
+                    Color.skyBlueToNavy
+                case 3:
+                    Color.realOrangeToRoseRed
+                case 4:
+                    Color.purpleToJetBlack
+                case 5:
+                    Color.limeGreenToDarkGreen
+                case 6:
+                    Color.pinlToSkyBlue
+                case 7:
+                    Color.pureWhiteToPurple
+                case 8:
+                    Color.realOrangeToNavy
+                case 9:
+                    Color.jetBlackToSkyBlue
+                default:
+                    Color.clear
+                }
+
+            } else {
+                // 色変えるビビット
+                switch count % 10 {
+                case 0:
+                    Color.black
+                case 1:
+                    Color.red
+                case 2:
+                    Color.gray
+                case 3:
+                    Color.blue
+                case 4:
+                    Color.green
+                case 5:
+                    Color.orange
+                case 6:
+                    Color.yellow
+                case 7:
+                    Color.brown
+                case 8:
+                    Color.cyan
+                case 9:
+                    Color.indigo
+                default:
+                    Color.clear
+                }
+            }
+        } else {
+            if counterModel.isGradation {
+                // 色変えないグラデーション
+                Color.allGradation
+            } else {
+                // 色変えないビビット
+                Color.darkBrown
+            }
         }
     }
 }
