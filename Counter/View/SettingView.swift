d5b391cd0179a2c242529f2efb72986e8441a345
jin-kirino <jinkirino@gmail.com>
2023-06-04 12:57:36 +0900

    ヘッダーの文言を修正し、アプリのバージョンを追加

--- Counter/View/SettingView.swift
+++ Counter/View/SettingView.swift
@@ -14,7 +14,7 @@ struct SettingView: View {
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("設定")) {
+                Section(header: Text("背景色設定")) {
                     Toggle(isOn: $counterModel.isGradation) {
                         Text("グラデーション")
                     }
@@ -22,7 +22,17 @@ struct SettingView: View {
                         Text("色を変更")
                     }
                 }
+                Section(header: Text("情報")) {
+                    HStack {
+                        Text("バージョン")
+                        Spacer()
+                        Text("1.0(0)")
+                            .foregroundColor(Color.gray)
+                    }
+                }
             }
+            .navigationTitle("Counter")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
