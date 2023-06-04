011b16771dbedcd1143c8b09d597803a88af03a7
jin-kirino <jinkirino@gmail.com>
2023-06-04 13:14:57 +0900

    paddingを修正

--- Counter/View/ContentView.swift
+++ Counter/View/ContentView.swift
@@ -77,6 +77,7 @@ struct ContentView: View {
                     Circle()
                         .stroke(Color.white, lineWidth: 3)
                 )
+                .padding(.bottom)
                 .padding(.leading)
         }
     }
